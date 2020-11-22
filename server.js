37adbbe8934dc28f489f09bee459f218e333b1d0
Florian Soller <flsoller@gmail.com>
2020-11-22 17:29:50 +0100

    Add undhandled rejection handler.

--- server.js
+++ server.js
@@ -23,9 +23,19 @@ if (process.env.NODE_ENV === 'development') {
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+// Port setup
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+// Run server
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
