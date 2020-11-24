08337b1187f9926469988c82a1b6458f0bd12743
Florian Soller <flsoller@gmail.com>
2020-11-24 20:26:09 +0100

    Create error handler.

---
+++ middleware/error.js
@@ -0,0 +1,11 @@
+const errorHandler = async (err, req, res, next) => {
+  // Log to console for dev
+  console.log(err.stack);
+
+  res.status(err.statusCode || 500).json({
+    success: false,
+    err: err.message || 'Server Error',
+  });
+};
+
+module.exports = errorHandler;
